4e9e44a6d91282a5815ae073abf418dda5973471
Imaad Malik <imaadmalik2@gmail.com>
2023-04-16 20:38:34 -0400

    Progressed with main.js and index.html

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+let color = black;
+
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
-    console.log("yo")
+
+    let btn_popup = document.querySelector('#popup')
+    btn_popup.addEventListener('click', function(){
+        let size = getSize();
+        createBoard(size);
+    })
+    console.log("yo");
 })
 
 function createBoard(size) {
@@ -13,7 +21,26 @@ function createBoard(size) {
 
     for(let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
-       
+        div.addEventListener('mouseover', colorDiv())
         board.insertAdjacentElement("beforeend", div);
     }
+}
+
+function getSize() {
+    let input = prompt('Give a size of the board. The number used will be the width and height of the board')
+    let message = document.querySelector('#message');
+    if(input == '') {
+        message.innerHTML = 'Please provide a number';
+    }
+    else if (input < 0 || input > 100){
+        message.innerHTML = 'Provide a number between 1 and 100';
+    }
+    else {
+        message.innerHTML = 'Now you can play';
+        return input;
+    }
+}
+
+function colorDiv(colorChoice) {
+    let color = colorChoice;
 }
